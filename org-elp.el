5c3930080d0e4fa1b3001c937a25126afa3bd27b
Yilun Guan <zoom.aaron@gmail.com>
2021-01-26 23:46:05 -0500

    conform style according to check-doc

--- org-elp.el
+++ org-elp.el
@@ -1,4 +1,4 @@
-;; org-elp.el 	-*- lexical-binding: t -*-
+;;; org-elp.el --- Preview latex equations in org mode while editing
 
 ;; Author: Yilun Guan
 ;; URL: https://github.com/guanyilun/org-elp
@@ -24,7 +24,7 @@
 ;;; Commentary:
 ;;
 ;; Preview latex equations in org mode in a seperate buffer while
-;; editing. Possible configurations:
+;; editing.  Possible configurations:
 ;;
 ;; To adjust split size:
 ;; (setq org-elp-split-size 30)
@@ -40,22 +40,21 @@
 ;; (require 'posframe)  ;; not working
 
 (defgroup org-elp nil
-  "org-z customizable variables."
+  "org-elp customizable variables."
   :group 'org)
 
 (defcustom org-elp-buffer-name "*Equation Live Preview*"
-  "Buffer name used to show the previewed equation"
+  "Buffer name used to show the previewed equation."
   :type  'string
   :group 'org-elp)
 
-(defcustom org-elp-split-size 30
-  "Split window size for displaying equation, this number is the
-number of lines in the main text remaining after the split"
+(defcustom org-elp-split-size 20
+  "Split window size for displaying equation, this number is the number of lines in the main text remaining after the split."
   :type  'integer
   :group 'org-elp)
 
 (defcustom org-elp-idle-time 0.5
-  "Idle time after editing to wait before creating the fragment"
+  "Idle time after editing to wait before creating the fragment."
   :type  'float
   :group 'org-elp)
 
@@ -64,6 +63,7 @@ number of lines in the main text remaining after the split"
 (defvar org-elp--preview-buffer nil)
 
 (defun org-elp-preview ()
+  "Preview the equation at point in buffer defined in `org-elp-buffer-name'."
   (let ((datum (org-element-context)))
     (and (memq (org-element-type datum) '(latex-environment latex-fragment))
          (let* ((beg (org-element-property :begin datum))
@@ -74,19 +74,8 @@ number of lines in the main text remaining after the split"
                (erase-buffer) (insert (replace-regexp-in-string "\n$" "" text))
                (org-preview-latex-fragment)))))))
 
-;; FIXME: not working, image not showing, otherwise it will be better
-;; than a split. There's also a problem with cursor jumping around with
-;; posframe which is a bit annoying.
-;; (defun org-elp-preview-with-posframe ()
-;;   ;; (posframe-hide-all)
-;;   (org-elp-preview)
-;;   (when (posframe-workable-p)
-;;     (posframe-show org-elp-buffer-name)))
-
-(defun org-elp-preview-with-splits ()
-  (org-elp-preview))
-
 (defun org-elp-open-buffer ()
+  "Open the preview buffer."
   (split-window-vertically org-elp-split-size)
   (other-window 1)
   (switch-to-buffer org-elp-buffer-name)
@@ -95,20 +84,22 @@ number of lines in the main text remaining after the split"
 
 ;;;###autoload
 (defun org-elp-activate ()
+  "Activate previewing.  This launches the idle timer and preview equations after idle time defined in `org-elp-idle-time'."
   (interactive)
   (message "Activating org-elp")
   (setq org-elp--org-buffer (current-buffer)
         org-elp--preview-buffer (get-buffer-create org-elp-buffer-name))
   (org-elp-open-buffer)
   (setq org-elp--timer (run-with-idle-timer
-                        org-elp-idle-time t 'org-elp-preview-with-splits)))
+                        org-elp-idle-time t 'org-elp-preview)))
 
 ;;;###autoload
 (defun org-elp-deactivate ()
+  "Deactivate previewing.  This remove the idle timer."
   (interactive)
   (delete-other-windows)
   (message "Deactivating org-elp")
-  (cancel-function-timers 'org-elp-preview-with-splits))
+  (cancel-function-timers 'org-elp-preview))
 
 ;;;###autoload
 (define-minor-mode org-elp
@@ -121,4 +112,4 @@ number of lines in the main text remaining after the split"
     (org-elp-deactivate)))
 
 (provide 'org-elp)
-;; org-elp ends here
+;;; org-elp ends here
